d6affa069ceabe58ceadcb76e1420d83bc5c2025
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-03-26 22:31:01 +0900

    Update Cubic Root.py

--- SoloLearn/Cubic Root.py
+++ SoloLearn/Cubic Root.py
@@ -1,7 +1,7 @@
 basecubics=[x**3 for x in range(0,11)]
 print(basecubics)
-LIMIT=0.0001
-sign=0 #0: Positive, 1 Negative number
+
+# sign=0 #0: Positive, 1 Negative number
 #----------------------------------------------------
 def Analyse(nstr):
     #save sign state and extract int part of number
@@ -16,61 +16,50 @@ def Analyse(nstr):
     #cal tree digits multiple times
     times0=(len(positive)-1)//3
     cubic_base=positive[:len(positive)-times0*3]
-    return abs(num0),positive,cubic_base,times0,signal
+    return abs(num0),cubic_base,times0,signal
 #-------------------------------------------------------
-def SearchBound(realNum,basecubic,times10):
-    isRealCubic=False
+def isPerfectCubic(realNum,basecubic,times10):
+    isCubic=False
+    downbound=realNum
     for i in range(0,11):
         if basecubics[i] == int(basecubic):
             compareNum=int(basecubic)*10**(3*times10)
             if (float(realNum)-compareNum) == 0:
-                isRealCubic=True
-                upbound=i*10**times10
-                downbound=upbound
-            else:
+                isCubic=True
                 downbound=i*10**times10
-                upbound=(i+1)*10**times10
-            break
-        elif basecubics[i]>int(basecubic):
-            upbound=i*10**times10
-            downbound=(i-1)*10**times10
-            break
-    return downbound,upbound,isRealCubic
-def RootCal(minLimit,maxLimit,number,times_10):
-    found=False
-    ans=minLimit
-    count=0
-    while  not found:
-        temp1=minLimit**3
-        temp2=maxLimit**3
-        delta1=abs(number-temp1)
-        delta2=abs(temp2-number)
-        print(temp1,temp2,delta1,delta2,count)
-        if delta1 <= LIMIT:
-            found=True
-            ans=minLimit
-        elif delta2 <=LIMIT:
-            found=True
-            ans=maxLimit
-        else:
-            count +=1
-            mid=(minLimit+maxLimit)/2
-            if delta1>delta2:
-                minLimit=mid
-            elif delta1<delta2:
-                maxLimit=mid
-            else:
-                minLimit=mid/3
-            continue
-    return ans
-#-------------------------------------------------------------
+                break
+    return isCubic,downbound
+#-----------------------------------------------------
+LIMIT =0.0001
+def epsilon(n, mid) : 
+    if (n > (mid * mid * mid)) : 
+        return (n - (mid * mid * mid)) 
+    else : 
+        return ((mid * mid * mid) - n) 
+#------------------------------------------------------
+def cubicRoot(n) : 
+    start = 0
+    end = n 
+    while True : 
+        mid = (start + end) / 2
+        error = epsilon(n, mid) 
+        if (error <= LIMIT) : 
+            return mid 
+        if ((mid * mid * mid) > n) : 
+            end = mid 
+        else : 
+            start = mid 
+#------------------------------------------------------
 num_str= input('input number: ').strip()
-num,int_num,base_num,times,sign=Analyse(num_str)
-# print(num,int_num,base_num,times,sign)
-min,max,isCompleteCubic=SearchBound(num,base_num,times)
-print(min,max)
-if isCompleteCubic:
-    cubicRoot=min*((-1)**sign)
+num,base_num,times,sign=Analyse(num_str)
+perfectCubic,cubic=isPerfectCubic(num,base_num,times)
+if perfectCubic:
+    print("Cubic root of", num_str, "is",  cubic*(-1)**sign)
 else:
-    cubicRoot=RootCal(min,max,num,base_num)
-print('Cubic Root of {0} is {1}'.format (num_str,cubicRoot))
+    if num<1:
+        num=num*1000
+        ans=cubicRoot(num)  
+        print("Cubic root of", num_str, "is",  round(ans/10,5)*(-1)**sign)
+    else:
+        ans=cubicRoot(num)  
+        print("Cubic root of", num_str, "is",  round(ans,5)*(-1)**sign)
