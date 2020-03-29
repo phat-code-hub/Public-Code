671463096b3a4ecdd393c70f4a115c55b5e6a533
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-03-29 14:48:20 +0900

    Create Prime Definition

---
+++ Algorithm/Prime.py
@@ -0,0 +1,19 @@
+def isPrime(n):
+    """ Confirm n is prime number or not """
+    ans=True
+    for i in range(2,n):
+        if n % i == 0:
+            ans=False
+            break
+    return ans
+def PrimeInRange(min=1,max=100,isprint=True):
+    """ List prime numbers in min and max range """
+    list=[]
+    if min==0:
+        min=1
+    for n in range(min,max+1):
+        if isPrime(n):
+            list.append(n)
+    if isprint:
+        for n in list:
+            print(n)
