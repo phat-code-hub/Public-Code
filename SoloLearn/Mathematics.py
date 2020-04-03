bab12c3028adca37f0120782f88014f5f27985c7
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-03 19:01:25 +0900

    Update Mathematics.py

--- SoloLearn/Mathematics.py
+++ SoloLearn/Mathematics.py
@@ -1,10 +1,7 @@
 number=int(input('So :'))
 exprs=input('Chuoi :').strip().split()
-for i in range(1, len(exprs)):
-    index=-1
+for i in range(0, len(exprs)):
     value= eval(exprs[i])
     if value == number:
-        index=i
+        print('index ', i)
         break
-if index>=0:
-    print('index ', index)
