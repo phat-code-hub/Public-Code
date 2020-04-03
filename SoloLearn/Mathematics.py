ccf5454e2345f1bdac5f484104c6588fd0ddca17
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-03 20:40:36 +0900

    update mathematics

--- SoloLearn/Mathematics.py
+++ SoloLearn/Mathematics.py
@@ -1,7 +1,12 @@
 number=int(input('So :'))
 exprs=input('Chuoi :').strip().split()
+ans=-1
 for i in range(0, len(exprs)):
     value= eval(exprs[i])
     if value == number:
-        print('index ', i)
+        ans=i
         break
+if ans >=0:
+    print('index',ans)
+else:
+    print('none')
