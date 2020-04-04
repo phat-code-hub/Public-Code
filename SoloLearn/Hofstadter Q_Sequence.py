527eb18412adfb43423c8b5e07bcd7c9b71f8b49
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-04 17:07:41 +0900

    Update Hofstadter Q_Sequence.py

--- SoloLearn/Hofstadter Q_Sequence.py
+++ SoloLearn/Hofstadter Q_Sequence.py
@@ -3,14 +3,15 @@ def func(n):
         return 1
     else:
         return func(n-func(n-1))+func(n-func(n-2))
-number= input('So: ')
+#------------------------------------------------
+number_str= input('So: ')
 try:
-    if number.isdecimal:
-        if int(number)==1 or int(number)==2:
-            print(1)
-        elif int(number)>2:
-            print(func(int(number))) 
-        else:
-            print('Negative number')
+    number=int(number_str)
+    if number <=0:
+        print('Not positive integer!')
+    elif number==1 or number==2:
+        print(1)
+    else :
+        print(func(int(number))) 
 except:
     print('Invalid number')
