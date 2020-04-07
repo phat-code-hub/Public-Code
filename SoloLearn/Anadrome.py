731825b5edfc59369dae2115ef4b3a299209f1f4
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-07 21:56:51 +0900

    Update Anadrome.py

--- SoloLearn/Anadrome.py
+++ SoloLearn/Anadrome.py
@@ -1,11 +1,39 @@
+#This solution based on the reasons below:
+#1)If the length of palindrome is even:
+#     all letters of word must have even frequency of apperance 
+#     (called : repeat times) for example in 'SolooloS' : S :2 , o :4,l:2
+#2)If the length of palindrome is odd:
+#    one (and only one) letter has odd repeat time ('madam','mamam')
+#3)All rearranging word in same anadrome has unique length and also has
+#   same letter characteristic (kind , repeate time)
+# => we don mind what the given phase is but only need to know the  
+#  frequency of the letters appear in the word to decide this phrase
+# would be a anadrome or not
+
 from collections import Counter
-#word=input('Word: ').strip()
-word="racecar"
-length=len(word)
-num=list(Counter(word).values())
-print(num)
-# if length % 2 ==0:
-#     pass
-# else:
-#     ls=[n for n in repeat_times.values() if n % 2 == 0]
-#     print(len(ls))
+def Analyse(phrase):
+    ans='no'
+    length=len(phrase)
+    num=list(Counter(phrase).values())
+    IsEven=[n % 2 ==0 for n in num]
+    Chk_All_Even=all(cond==True for cond in IsEven)
+    odd_count=IsEven.count(False)
+    if length % 2 ==0:
+        if Chk_All_Even:
+            ans= 'yes'
+    else:
+        if odd_count ==1:
+            ans='yes'
+    return ans
+#----------------------------------------------
+words=input('phrase: ').strip().split()
+if len(words) ==1:
+    print(Analyse(words[0]))
+else:
+    answer=[]
+    for word in words:
+        answer.append(Analyse(word))
+    if all(a == 'yes' for a in answer):
+        print('yes')
+    else:
+        print('no')
