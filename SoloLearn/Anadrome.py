f43564b9bc2649c4eb9fbc61358c23eeccdd653f
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-07 20:55:22 +0900

    Update Anadrome.py

--- SoloLearn/Anadrome.py
+++ SoloLearn/Anadrome.py
@@ -1,18 +1,11 @@
-def Analyse(data):
-    chuoi=[]
-    Tu=data
-    l=len(Tu)
-    if l>=3:
-        for i in range(l):
-            first=Tu[0]
-            left=Tu[1:]
-            chuoi.append(first+left)
-            Tu=left+first
-            l=len(Tu)
-    else:
-        print('too small')
-    return chuoi
-#word=input('String: ').strip()
-word='abcd'
-res=Analyse(word)
-print(res)
+from collections import Counter
+#word=input('Word: ').strip()
+word="racecar"
+length=len(word)
+num=list(Counter(word).values())
+print(num)
+# if length % 2 ==0:
+#     pass
+# else:
+#     ls=[n for n in repeat_times.values() if n % 2 == 0]
+#     print(len(ls))
