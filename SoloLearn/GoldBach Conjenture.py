f9eb2603bdf2c16299808a806bc047df9a65e9dc
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-07 18:44:21 +0900

    Update GoldBach Conjenture.py

--- SoloLearn/GoldBach Conjenture.py
+++ SoloLearn/GoldBach Conjenture.py
@@ -1,34 +1,40 @@
 import math
 from sympy import sieve
-def OneNumber(n):
+def OneNumber(n,isRange=False):
     n1=[]
     n2=[]
+    num_list=[]
     prime_list=[x for x in sieve.primerange(1,n)]
     for i in prime_list:
         if (n-i) in prime_list:
             n1.append(i)
             n2.append(n-i)
+            num_list.append(n)
     if len(n1) % 2 ==0:
         limit=len(n1)//2
     else:
         limit=len(n1)//2+1
-    return n1[:limit],n2[:limit]
-def RangeNumber(n1,n2):
-    for i in range(n1,n2,2):
-        pass
-def Show(NumStr,n1,n2,isRange=False):
+    n1=n1[:limit]
+    n2=n2[:limit]
+    num_list=num_list[:limit]
     if isRange:
-        pass
-    else:
+         for i in range(len(n1)):
+            print('{0}: {1} + {2}'.format(num_list[i],n1[i],n2[i]))
+    else:    
         for i in range(len(n1)):
             print('{0} + {1}'.format(n1[i],n2[i]))
+def RangeNumber(n1,n2):
+    numbs=[]
+    num1_ls=[]
+    num2_ls=[]
+    for i in range(n1,n2+1,2):
+        OneNumber(i,True)
 try:
     num_str = input('Number(s): ').strip().split(',')
     if len(num_str) ==1 :
         num=int(num_str[0])
         assert num % 2 == 0   ,'Number must be even!'
-        num1,num2=OneNumber(num)
-        Show(num_str,num1,num2,False)
+        OneNumber(num)
     else:
         num1_str,num2_str=num_str
         number1=int(num1_str)
@@ -37,7 +43,8 @@ try:
         if number2<number1:
             number1,number2=number2,number1
         if number1 == number2:
-            pass
-        num1,num2,list =RangeNumber(number1,number2)
+            OneNumber(number1)
+        else:
+            RangeNumber(number1,number2)
 except:
     print('Invalid Even Number ')
