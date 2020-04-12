922722671da4923c1960cd04d284dd05de637ee3
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-12 13:54:11 +0900

    Update Land Ho.py

--- SoloLearn/Land Ho.py
+++ SoloLearn/Land Ho.py
@@ -2,8 +2,8 @@ WAITTING_TIME=10
 LIMIT_PEOPLE=20
 people_ahead=int(input('waitting people: '))
 if (people_ahead+1) % LIMIT_PEOPLE ==0:
-    board_ship_order=(people_ahead+1) // LIMIT_PEOPLE
+    board_waitting=people_ahead // LIMIT_PEOPLE
 else:
-    board_ship_order=(people_ahead+1) // LIMIT_PEOPLE +1
-time_to_board=board_ship_order*WAITTING_TIME
-print(time_to_board)
+    board_waitting=(people_ahead+1) // LIMIT_PEOPLE
+time_to_Beach=board_waitting*WAITTING_TIME*2+WAITTING_TIME
+print(time_to_Beach)
