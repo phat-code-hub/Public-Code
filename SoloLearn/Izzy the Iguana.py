1f1aef9312922175ffa507ccbbd5c3bc4d058473
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-12 13:32:05 +0900

    Update Codes

--- SoloLearn/Izzy the Iguana.py
+++ SoloLearn/Izzy the Iguana.py
@@ -1,9 +1,8 @@
 ENOUGH_POINT=10
 snack_points={'Lettuce':5,'Carrot':4,'Mango':9,'Cheeseburger':0}
-print(snack_points['Cheeseburger'])
 snacks=input('Total snack: ').strip().split()
 sum=0
-for snack in snacks:
-    sum += snack_points[snack]
-print(sum)
-print('Come on Down' if sum>=ENOUGH_POINT else 'Time to wait')
+if len(snacks) >0:
+    for snack in snacks:
+        sum += snack_points[snack.capitalize()]
+print('Come on Down!' if sum>=ENOUGH_POINT else 'Time to wait')
