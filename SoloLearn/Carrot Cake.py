7a3396caf45ce1f6b96b847a98c35ca073ed5791
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-12 14:17:11 +0900

    Update Carrot Cake.py

--- SoloLearn/Carrot Cake.py
+++ SoloLearn/Carrot Cake.py
@@ -2,11 +2,7 @@ CARROT=7
 carrot_num=int(input('carrot: '))
 box_num=int(input('box: '))
 leftover=carrot_num % box_num
-if leftover==0:
-    need_to_buy=CARROT
-else:
-    need_to_buy=CARROT-(leftover % CARROT)
-if need_to_buy ==0:
+if leftover>=CARROT:
     print('Cake Time')
 else:
-    print('I need to buy {carrot} more'.format(carrot=need_to_buy))
+    print('I need to buy {carrot} more'.format(carrot=CARROT-leftover))
