8a106cf382636e4a3762597be333d01f8033bf2f
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-15 21:05:07 +0900

    Update Kaprekar constant Easy.py

--- SoloLearn/Kaprekar constant Easy.py
+++ SoloLearn/Kaprekar constant Easy.py
@@ -1,18 +1,24 @@
+from collections import Counter
+def checkNumber(word):
+    return len(word)==4 and len(Counter(word))>1
+def getResult(str_of_number):
+    asc_number=''.join(sorted(str_of_number))
+    desc_number=asc_number[::-1]
+    return str(int(desc_number)-int(asc_number))
 try:
     num_str= input('Four digits number: ')
-    assert len(num_str)==4
-    test_num_str=num_str
-    before_result=int(test_num_str)
-    after_result=0
+    assert checkNumber(num_str)
+    before_calc=num_str
+    after_calc=0
+    count=1
     while True:
-        desc_num=int(''.join(sorted(str(before_result),reverse=True)))
-        asc_num=int(''.join(sorted(str(before_result))))
-        after_result=desc_num-asc_num
-        if after_result == before_result:
+        after_calc=getResult(before_calc)
+        if after_calc == before_calc:
+            print('the Kaprekar\'s constant is {0} after {1} repeat times'.format(after_calc,count))
             break
         else:
-            before_result=after_result
-
+            count+=1
+            before_calc=after_calc
 except :
     print('Invalid Number')
-
+    print('number is 4 degits and at least 2 different numbers')
