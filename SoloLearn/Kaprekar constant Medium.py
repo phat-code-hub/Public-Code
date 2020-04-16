753b84c2e9527dc1b946cca54615263692ee3aad
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-16 20:49:36 +0900

    Update Kaprekar constant Medium.py

--- SoloLearn/Kaprekar constant Medium.py
+++ SoloLearn/Kaprekar constant Medium.py
@@ -1,5 +1,6 @@
 from collections import Counter
-def checkValidNumber(num_str):
+#-----------------------------------------------------------------------------------------
+def checkValid(num_str):
     repeat_list=Counter(num_str)
     isValid=False
     # no repetition for 3 degits number
@@ -17,5 +18,32 @@ def checkValidNumber(num_str):
         if max(repeat_list.values()) ==2 or len(repeat_list)==4:
             isValid=True
     return isValid
-num =input('input any number in range(101 ~ 9999): ' ).strip()
-print(checkValidNumber(num))
+#-----------------------------------------------------------------------------------------
+def calculate(str_of_number,step_count):
+    asc_num=''.join(sorted(str_of_number))
+    desc_num=asc_num[::-1]
+    result= int(desc_num)-int(asc_num)
+    print('Step {0} : {1} - {2} = {3}'.format(step_count,desc_num,asc_num,result))
+    return str(result)
+#-----------------------------------------------------------------------------------------
+try:
+    num_str =input('input any number in range(101 ~ 9999): ' ).strip()
+    assert int(num_str)>100 and int(num_str)<10000
+    if checkValid(num_str) :
+        before=num_str
+        after=0
+        step=1
+        print()
+        while True:
+            after=calculate(before,step)
+            if after == before:
+                print()
+                print('After {0} steps ,the Kaprekar\'s constant of {1} approached {2}.'.format(step,num_str,after))
+                break
+            else:
+                step+=1
+                before=after
+    else:
+        print('This  number is too repeatitons')
+except :
+    print('Invalid number')
