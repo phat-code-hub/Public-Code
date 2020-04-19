0264aed30aa0282b14e2507b6aa5ccfc8ad0b122
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-19 13:28:27 +0900

    Update Halloween Candy.py

--- SoloLearn/Halloween Candy.py
+++ SoloLearn/Halloween Candy.py
@@ -1,14 +1,19 @@
 try:
     houses=int(input('House visited: '))
     assert houses>=3
-    mod=houses % 3
-    div= houses //3
-    # the same code :div,mod=divmod(houses,3)
-    if mod ==2: # gift may be dollar bills 
-        dollar_Case=div*2+2
-    else: # toothbrushes only
-        dollar_Case=div*2
+    if houses==3: # 3:do not give candy, 4: give candy, toothbrushes and dollar
+        dollar_Case=2
+    else: # > 4
+        dollar_Case=2
+        # mod=houses % 4
+        # div= houses //4
+        # # the same code :div,mod=divmod(houses,3)
+        # if mod==3: # can give dollar
+        #     dollar_Case=div*2+2
+        # else:
+        #     dollar_Case=div*2
     percentage=round((dollar_Case/houses)*100)
     print(percentage)
 except:
     print('Invalid appropriate integer number')
+    print('Visited houses must be more than 2')
