2435dbb8f884d6f61241dfa5aff5744b20ff7a6f
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-19 14:54:16 +0900

    Update No Numerals.py

--- SoloLearn/No Numerals.py
+++ SoloLearn/No Numerals.py
@@ -8,27 +8,31 @@ table={'10': 'ten','0':'zero','1':'one','2':'two','3':'three','4':'four',
        '5':'five', '6':'six','7':'seven','8':'eight','9':'nine'}
 reg_null=re.compile(r'(.)') #Not null
 reg_decimal=re.compile(r'[0-9]+') # have at least one numeric letter
+reg_zero=re.compile(r'(0+\d)*') # find nosense zeros
 #-----------------------------------------------------------------------
 def checkValidString(data):
     return reg_null.match(data) and reg_decimal.search(data)
 #-----------------------------------------------------------------------
-def Convert_Num_toString(data):
-    words=data.split()
-    converted_phrase=''
-    for w in words:
-        if not w.isalpha(): # have decimal letter
-            w=str(int(w)) # remove cases like '01'
-        for k in table:
-            if k in w:
-                w=w.replace(k,table[k])
-                break
-        converted_phrase+=w+ ' '
-    return converted_phrase
+def remove_Zero(data):
+    m=reg_zero.findall(data)
+    res=data
+    if m:
+        list_zero=[s for s in list(m) if s!='']
+        if len(list_zero)>0:
+            for s in list_zero:
+                res=res.replace(s,str(int(s)))
+    return res
 #-----------------------------------------------------------------------
 #Main code
 try:
     phrase=input('Phrase :').strip().lower()
     assert checkValidString(phrase)
-    print(Convert_Num_toString(phrase))
+    #remove nonsense zeros like '01' 
+    phrase=remove_Zero(phrase)
+    #replace all numeric words by appropriate term 
+    for num in table:
+        if num in phrase:
+            phrase=phrase.replace(num,table[num],len(phrase))
+    print(phrase)
 except:
     print('Phrase was nothing or had not any number!')
