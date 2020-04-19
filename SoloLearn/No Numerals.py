8406bd3e97985407801c9916647dec51ea277df6
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-19 13:22:25 +0900

    Update No Numerals.py

--- SoloLearn/No Numerals.py
+++ SoloLearn/No Numerals.py
@@ -6,7 +6,7 @@
 import re
 table={'10': 'ten','0':'zero','1':'one','2':'two','3':'three','4':'four',
        '5':'five', '6':'six','7':'seven','8':'eight','9':'nine'}
-reg_null=re.compile(r'.*') #Not null
+reg_null=re.compile(r'(.)') #Not null
 reg_decimal=re.compile(r'[0-9]+') # have at least one numeric letter
 #-----------------------------------------------------------------------
 def checkValidString(data):
@@ -16,6 +16,8 @@ def Convert_Num_toString(data):
     words=data.split()
     converted_phrase=''
     for w in words:
+        if not w.isalpha():
+            w=str(int(w)) # remove cases like '01'
         for k in table:
             if k in w:
                 w=w.replace(k,table[k])
