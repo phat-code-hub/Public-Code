210e7987944a37477e1d4db59b18b2da846abedf
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-19 13:08:40 +0900

    Update No Numerals.py

--- SoloLearn/No Numerals.py
+++ SoloLearn/No Numerals.py
@@ -4,27 +4,29 @@
 #input : "Mike buys 3 apples and 4 bananas" (input words are original form (lowercase))
 #output : "Mike buys three apples and four bananas"
 import re
-table={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',
-        6:'six',7:'seven',8:'eight',9:'nine',10: 'ten'}
+table={'10': 'ten','0':'zero','1':'one','2':'two','3':'three','4':'four',
+       '5':'five', '6':'six','7':'seven','8':'eight','9':'nine'}
 reg_null=re.compile(r'.*') #Not null
 reg_decimal=re.compile(r'[0-9]+') # have at least one numeric letter
+#-----------------------------------------------------------------------
 def checkValidString(data):
     return reg_null.match(data) and reg_decimal.search(data)
 #-----------------------------------------------------------------------
-def convert_Num_toString(wordgroup):
-    converted_Sentence=''
-    for wd in wordgroup:
-        if reg_decimal.search(wd):
-            converted_Sentence += table[int(wd)]+' '
-        else: #no decimal
-            converted_Sentence+=wd.lower()+ ' '
-    return converted_Sentence
+def Convert_Num_toString(data):
+    words=data.split()
+    converted_phrase=''
+    for w in words:
+        for k in table:
+            if k in w:
+                w=w.replace(k,table[k])
+                break
+        converted_phrase+=w+ ' '
+    return converted_phrase
 #-----------------------------------------------------------------------
 #Main code
 try:
     phrase=input('Phrase :').strip().lower()
     assert checkValidString(phrase)
-    words=phrase.split()
-    print(convert_Num_toString(words))
+    print(Convert_Num_toString(phrase))
 except:
     print('Phrase was nothing or had not any number!')
