32eaff125736cfc50228f2d8ddbbee0dcef8cbfc
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-19 22:17:39 +0900

    Update No Numerals.py

--- SoloLearn/No Numerals.py
+++ SoloLearn/No Numerals.py
@@ -8,40 +8,33 @@ table={'10': 'ten','0':'zero','1':'one','2':'two','3':'three','4':'four',
        '5':'five', '6':'six','7':'seven','8':'eight','9':'nine'}
 reg_null=re.compile(r'(.)+') #Not null
 reg_decimal=re.compile(r'[0-9]+') # have at least one numeric letter
-reg_zero=re.compile('0+') # find nosense zeros
+reg_zero=re.compile(r'0+(?=[1-9])') # find nosense zeros
 #-----------------------------------------------------------------------
 def checkValidString(data):
     return reg_null.match(data) and reg_decimal.search(data)
 #-----------------------------------------------------------------------
 def remove_Zero(datas):
-    data=str(datas)
-    n1=0
-    n2=len(data)
+    data=datas
     while True:
-        m=reg_zero.search(data,n1,n2)
+        m=reg_zero.search(data)
         if m:
-            ch=''
-            if m.end()== n2 or data[m.end()]!='0':
-                if m.start()==n1 or data[m.start()-1]!='0':
-                    for i in range(m.start(),m.end()-1):
-                        ch+=data[i]
-                    data=data.replace(ch,'')
-            n1=m.start()
+            data=data[:m.start()]+data[m.end():]
         else:
             break
     return data
 #-----------------------------------------------------------------------
 #Main code
 try:
-    phrase=input('Phrase :').strip().lower()
-    assert checkValidString(phrase)
+    # phrase=input('Phrase :').strip().lower()
+    # assert checkValidString(phrase)
     #remove nonsense zeros like '01' 
+    phrase='00100d b0050f abc'
     phrase=remove_Zero(phrase)
     print('origin: ',phrase)
-    # #replace all numeric words by appropriate term 
-    # for num in table:
-    #     if num in phrase:
-    #         phrase=phrase.replace(num,table[num],len(phrase))
-    # print(phrase)
+    #replace all numeric words by appropriate term 
+    for num in table:
+        if num in phrase:
+            phrase=phrase.replace(num,table[num],len(phrase))
+    print(phrase)
 except:
     print('Phrase was nothing or had not any number!')
