5caaa8907857b798baa132fe9b55485653821020
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-24 20:30:22 +0900

    Delete LCF.py

--- SoloLearn/LCF.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sympy import sieve
-def findFactors(data):
-    print(len(data))
-input_list=input('Numbers :').strip()
-sep=',' if ',' in input_list else ' '
-numbers=list(map(int,input_list.split(sep)))
-findFactors(numbers)
