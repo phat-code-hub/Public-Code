57927542fe95813aab0ce67ee2afd1ed85ad4bf5
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-04-30 20:56:24 +0900

    Update Complex Number Multiplication and Division.py

--- SoloLearn/Complex Number Multiplication and Division.py
+++ SoloLearn/Complex Number Multiplication and Division.py
@@ -6,22 +6,37 @@
 #    =[(a1*b1+a2*b2)-(a1*b2-a2*b1)j]/(b1^2+b2^2)
 # => real_part of A/B : (a1*b1+a2*b2)/(b1^2+b2^2)
 #    imagine_part of A/B : -(a1*b2-a2*b1)/(b1^2+b2^2)
+#Note : This Code uses float number to calulate division 
+#------------------------------------------------------------------
 class complx():
     def __init__(self,real,img):
-        self.real=float(real)
-        self.img=float(img)
+        self.real=real
+        self.img=img
+    #Multiple define
     def mul(self,other):
         real_part=self.real*other.real - self.img*other.img
         img_part=self.real*other.img+self.img*other.real
         return complx(real_part,img_part)
+    #Division define
     def div(self,other):
         deno=other.real**2+other.img**2
-        real_part=float((self.real*other.real+self.img*other.img))
-        img_part=self.real*other.img-self.img*other*real
+        real_part=(self.real*other.real+self.img*other.img)/deno
+        img_part=(-1)*(self.real*other.img-self.img*other.real)/deno
         return complx(real_part,img_part)
-A=complx(1,2)
-B=complx(3,4)
-C=complx.mul(A,B)
-D=complx.div(B,A)
-print('{real}, {img}'.format(real=C.real,img=C.img))
-print('{real}, {img}'.format(real=D.real,img=D.img))
+#------------------------------------------------------------------
+def display(result):
+    LIMIT=5
+    print('{real}, {img}'.format(real=round(result.real,LIMIT),img=round(result.img,LIMIT)))
+#------------------------------------------------------------------
+try:
+    nums=input('four numbers:').strip().split(',')
+    A =complx(float(nums[0]),float(nums[1]))
+    B =complx(float(nums[2]),float(nums[3]))
+    C=complx.mul(A,B)
+    D=complx.div(A,B)
+    E=complx.div(B,A)
+    display(C)
+    display(D)
+    display(E)
+except :
+    print('Invalid')
