553042999aedbd1cdb7f73fe33c8f886698decad
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-03 14:26:00 +0900

    Update Digits of PI.py

--- SoloLearn/Digits of PI.py
+++ SoloLearn/Digits of PI.py
@@ -1,12 +1,22 @@
-def calculate(n):
-    tich=4/(8*n+1)-2/(8*n+4)-1/(8*n+5)-1/(8*n+6)
-    ans=16**(-n) * tich
-    return ans
-def pidigit(n):
-    res=0
-    for i in range(n):
-        res+=calculate(i)
-    return str(res)
-so=5
-hso=pidigit(so)
-print(hso)
+# """
+#This solution base on formular discovered by  David Bailey, Peter Borwein, and Simon Plouffe in 1995:
+# Pi = SUM from  k=0 to infinity of ( 16^(-k))*[ 4/(8k+1) – 2/(8k+4) – 1/(8k+5) – 1/(8k+6) ]
+# ref: https://math.hmc.edu/funfacts/finding-the-n-th-digit-of-pi/
+# """
+import math
+from decimal import Decimal as dec , getcontext
+getcontext().prec = 1002 # precision of display
+def findN():
+    #change to decimal since this formular return hex  numbers
+    pi_sum = dec(0)
+    for k in range(1001): 
+        pi_sum += dec(math.pow(16, -k)) * (dec(4/(8*k+1)) - dec(2/(8*k+4)) - dec(1/(8*k+5)) - dec(1/(8*k+6)))
+    return str(pi_sum)[2:] # Take digits after decimal point
+#------------------------------------------------------------------------------------------
+try:
+    nth= int(input(('N= ')))
+    assert 0 < nth< 1001
+    pi_Nth=findN()
+    print(pi_Nth[nth-1])
+except :
+    print('Invalid!')
