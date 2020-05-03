6dc083814fd237e4402450d2f7b765cde76c2da2
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-03 14:49:58 +0900

    Add New Code

--- SoloLearn/Digits of PI.py
+++ SoloLearn/Digits of PI.py
@@ -5,11 +5,11 @@
 # """
 import math
 from decimal import Decimal as dec , getcontext
-getcontext().prec = 1002 # precision of display
+getcontext().prec = 10000 # precision of display
 def findN():
     #change to decimal since this formular return hex  numbers
     pi_sum = dec(0)
-    for k in range(1001): 
+    for k in range(10000): 
         pi_sum += dec(math.pow(16, -k)) * (dec(4/(8*k+1)) - dec(2/(8*k+4)) - dec(1/(8*k+5)) - dec(1/(8*k+6)))
     return str(pi_sum)[2:] # Take digits after decimal point
 #------------------------------------------------------------------------------------------
@@ -17,6 +17,7 @@ try:
     nth= int(input(('N= ')))
     assert 0 < nth< 1001
     pi_Nth=findN()
+    print(pi_Nth[:44])
     print(pi_Nth[nth-1])
 except :
     print('Invalid!')
