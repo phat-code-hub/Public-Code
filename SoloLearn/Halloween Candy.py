0037fcf94b59b4f3dda8b4f86dbe428cad90d87e
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-09 17:15:05 +0900

    renew codes

--- SoloLearn/Halloween Candy.py
+++ SoloLearn/Halloween Candy.py
@@ -1,17 +1,9 @@
+import math
 try:
     houses=int(input('House visited: '))
     assert houses>=3
     dollar=2
-    if houses==3:
-        percent=0
-    elif houses==4:
-        percent=50
-    else:
-        le=houses //10
-        chia=(houses -4) % 3
-        if chia  ==2 :
-            dollar+=chia*le
-        percent=round(dollar/houses *100)
+    percent=math.ceil(dollar/houses *100)
     print(percent)
 except:
     print('Invalid appropriate integer number')
--- SoloLearn/No Numerals.py
+++ SoloLearn/No Numerals.py
@@ -1,4 +1,4 @@
-#This program replaces numbers (0-9) written in the sentence 
+#This program replaces numbers (0-10) written in the sentence 
 #into suitale English word
 #example:
 #input : "Mike buys 3 apples and 4 bananas" (input words are original form (lowercase))
@@ -8,6 +8,7 @@ table={'10':'ten','0':'zero','1':'one','2':'two','3':'three','4':'four',
        '5':'five', '6':'six','7':'seven','8':'eight','9':'nine'}
 reg_null=re.compile(r'(.)+') #Not null
 reg_decimal=re.compile(r'[0-9]+') # have at least one numeric letter
+reg_zero=re.compile(r'[2-9]+(?!0)')
 #-----------------------------------------------------------------------
 def checkValidString(data):
     return reg_null.match(data) and reg_decimal.search(data)
@@ -16,8 +17,17 @@ def checkValidString(data):
 try:
     phrase=input('Phrase :').strip().lower()
     assert checkValidString(phrase)
-    for num in table:
-         phrase=phrase.replace(num,table[num],len(phrase))
+    # for num in table:
+    #      phrase=phrase.replace(num,table[num],len(phrase))
+    # print(phrase)
+    phrases=phrase.split()
+    mang=[]
+    for w in phrases:
+        l=w
+        if w.isdecimal() and int(w)<=10:
+            l=table[w]
+        mang.append(l)
+    phrase=' '.join(mang)
     print(phrase)
 except:
     print('Phrase was nothing or had not any number!')
