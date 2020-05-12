6bcab1e3b06870aa970ec5b98a1a768e681578f5
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-12 22:01:14 +0900

    Update Rotate.java

--- Java Code/Rotate.java
+++ Java Code/Rotate.java
@@ -3,19 +3,25 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Rotate {
-
+	static List<String> list;
+	static int n;
+	static int[][] mat;
+	static void InitMatrix() {
+		
+	}
 	public static void main(String[] args) {
 		System.out.print("Matrix Size: ");
 		Scanner sc= new Scanner(System.in);
-		String Nst=sc.nextLine();
-		int N=Integer.parseInt(Nst);
-		List<String> list=new ArrayList<>();
-		for (int l=0;l<N;l++) {
+		String nSt=sc.nextLine();
+		n=Integer.parseInt(nSt);
+		list=new ArrayList<>();
+		for (int l=0;l<n;l++) {
 			System.out.print("Row "+ (l+1)+"= ");
 			list.add(sc.nextLine());
 		}
 		sc.close();
-		System.out.print(list);
+//		System.out.print(list);
+		InitMatrix();
 
 	}
 
