3285f97d5dba1a2b342f2f7b126f0927bc00bc3d
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-14 23:02:07 +0900

    Update Rotate.java

--- Java Code/Rotate.java
+++ Java Code/Rotate.java
@@ -4,49 +4,53 @@ public class Rotate {
     static int size;
     static double OriginX,OriginY;
     static int[][] mat,matRot90,matRot270;
+  //---------------------------------------------------
     static void PrintMatrix(int[][] matrix ) {
         for (int i =0;i<size;i++) {
-            System.out.print("ROW "+(i+1)+": ");
             for (int j =0;j<size;j++) {
                 System.out.print(matrix[i][j]+ " ");
             }
             System.out.println();
         }
     }
+  //---------------------------------------------------
     static void RotateMatrix(int angle ) {
     	int x,y;
     	double x0,y0,fx,fy;
-    	switch(angle) {
-	    	case 270:
-	    		break;
-	    	default: //[0,2]
-	    		for (int i=0;i<size;i++) {
-	    			for(int j=0;j<size;j++) {
-	    				x0=i-OriginX; // 0-1=-1
-	    				y0=j-OriginY; // 2-1=1
-	    				fx=-y0; //-1
-	    				fy=x0; // -1
-	    				x=(int) (fx+OriginX); //0
-	    				y= (int) (fy+OriginY); //0
-	    				matRot90[x][y]=mat[i][j];
-	    			}
-	    		}
-    	
-    	}
+    	int signX,signY;
+    	int[][] tempMat=new int[size][size];
+    	signX=(angle == 90) ? -1:1;
+    	signY=(angle == 270) ? -1:1;
+    	for (int i=0;i<size;i++) {
+			for(int j=0;j<size;j++) {
+				x0=i-OriginX; 
+				y0=j-OriginY; 
+				fx=signX*y0; 
+				fy=signY*x0; 
+				x=(int) (fx+OriginX); 
+				y= (int) (fy+OriginY);
+				tempMat[x][y]=mat[i][j];
+			}
+		}
+    	if (angle ==270) matRot270= tempMat;
+    	else matRot90=tempMat;
     }
+  //---------------------------------------------------
     static void Init(String size_) {
     	size=Integer.parseInt(size_);
         mat=new int[size][size];
         matRot90=new int[size][size];
         matRot270=new int[size][size];
         if (size % 2 == 0 ) {
-        	OriginX=size /2;
-        	OriginY=size/2;
+        	OriginX=(double) (size-1) /2;
+        	OriginY=(double) (size-1)/2;
         }else {
-        	OriginX=(size-1) /2 +1;
-        	OriginY=(size-1) /2 +1;
+        	OriginX=(double) (size-1) /2 ;
+        	OriginY=(double) (size-1) /2 ;
         }
+        
     }
+  //---------------------------------------------------
     static void InitRow(int idx,String nums ) {
         nums=nums.trim();
         String[] rows=nums.split("[\\s,]+");
@@ -66,10 +70,14 @@ public class Rotate {
             InitRow(l,row);
         }
         sc.close();
+        System.out.println("Origin Martrix:");
         PrintMatrix(mat);
+        System.out.println("Rotate 90 degrees:");
         RotateMatrix(90);
         PrintMatrix(matRot90);
-        
+        System.out.println("Rotate 270 degrees:");
+        RotateMatrix(270);
+        PrintMatrix(matRot270);
     }
 
 }
