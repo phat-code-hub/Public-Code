18989a0830abbd55a5b21bb69dd56d788e55e79a
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-16 21:26:32 +0900

    Update Codes

--- Java Code/The_2D_Map.java
+++ Java Code/The_2D_Map.java
@@ -28,32 +28,33 @@ public class The_2D_Map {
 
 	public static void main(String[] args) {
 		try {
+			System.out.print("2D Array: ");
 			Scanner sc=new Scanner(System.in);
 			String map_=sc.nextLine().trim();
 			sc.close();
 			String[] map=map_.split(",");
-			Index firstP=new Index(-1,-1);
-			Index secondP=new Index(-1,-1);
+			Index firstP=new Index(0,0);
+			Index secondP=new Index(0,0);
 			boolean isP1=false;
 			boolean isP2=false;
 			for (int i=0;i<5;i++) {
 				String st=map[i];
-				int idx=-1;
-				while (true) {
-					if (st.indexOf("P",idx)>=0) {
+				int idx=0;
+				while (idx>=0) {
+					idx=st.indexOf("P",idx);
+					if (idx>=0) {
 						if (!isP1) {
 							firstP.setI(i);
 							idx=st.indexOf("P");
 							firstP.setJ( idx);
 							isP1=true;
-						} 
+						}
 						else if(isP1 & !isP2) {
 							secondP.setI(i);
 							secondP.setJ(st.indexOf("P"));
 							isP2=true;
 						} 
-						else break;
-					} else break;
+					}
 				}
 			}
 			if (isP1 & isP2) System.out.println(Index.totalMoves(firstP, secondP));
