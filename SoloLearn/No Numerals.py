eb81bd14e6b82cdfb1a19be51d486cf96f3cfcd6
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-17 11:05:17 +0900

    modify Codes

--- SoloLearn/No Numerals.py
+++ SoloLearn/No Numerals.py
@@ -6,47 +6,33 @@
 import re
 table={'10':'ten','0':'zero','1':'one','2':'two','3':'three','4':'four',
        '5':'five', '6':'six','7':'seven','8':'eight','9':'nine'}
-reg_null=re.compile(r'(.)+') #Not null
-reg_decimal=re.compile(r'[0-9]+') # have at least one numeric letter
-reg_num=re.compile(r'(\d)+')
+reg=re.compile(r'(\D)*\d+(\D)*')# have at least one decimal digit
+reg_num=re.compile(r'\d+')
 #-----------------------------------------------------------------------
 def checkValidString(data):
-    return reg_null.match(data) and reg_decimal.search(data)
+    return reg.match(data)   
 #-----------------------------------------------------------------------
-def extract(st):
-    a=[]
-    b=''
-    for i in st:
-        if i.isdecimal():
-            b+=i
-        else:
-            if len(b)>0:
-                if int(b)<=10:
-                    a.append(table[str(int(b))])
-                else:
-                    a.append(b)
-            b=''
-            a.append(i)
-    return ''.join(a)   
+def replaceNum(num_str):
+    numalfa=num_str
+    nums=reg_num.findall(numalfa)
+    if len(nums)>0:
+        for n in nums:
+            if int(n)<=10:
+                numalfa=numalfa.replace(n,table[n])
+    return numalfa
 #-----------------------------------------------------------------------
 #Main code
 try:
     phrase=input('Phrase :').strip().lower()
     assert checkValidString(phrase)
     phrases=phrase.split()
-    mang=[]
-    for w in phrases:
-        l=w
-        if reg_num.search(w):
-            if w.isnumeric():
-                if int(w) <=10:
-                    l=table[str(int(w))]
-                else:
-                    l=w
-            else:
-                l=extract(w)
-        mang.append(l)
-    phrase=' '.join(mang)
+    words=[]
+    for wd in phrases:
+        temp=wd
+        if wd.isalnum():
+            temp=replaceNum(wd)
+            words.append(temp)
+    phrase=(' ').join(words)
     print(phrase)
 except:
     print('Phrase was nothing or had not any number!')
