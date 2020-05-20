30ff4eaddf66b42fcb6e93bc6bee99882bbad6f7
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-20 18:41:26 +0900

    Update Average of Rows.py

--- SoloLearn/Average of Rows.py
+++ SoloLearn/Average of Rows.py
@@ -1,16 +1,14 @@
 import numpy as np
 try:
-    # dim=input('Dimension: ').split()
-    dim=input().split()
-    n,p=dim
+    n,p=[int(x) for x in input().split()]
     data=[]
-    for i in range(int(n)):
-        # datum=input('Row {}:'.format(i+1)).split()
+    for i in range(n):
         datum=input().split()
-        assert len(datum) ==int(p)
+        assert len(datum) ==p
         data +=datum
+    print(data)
     rows_=[float(r) for r in data]
-    rows=np.array(rows_).reshape(int(n),int(p))
+    rows=np.array(rows_).reshape(n,p)
     print(rows.mean(axis=1))
 except:
     print('Invallid')
