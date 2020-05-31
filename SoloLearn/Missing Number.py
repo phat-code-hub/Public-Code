ccf3bd617d649bedfd8c2de7e0110b2fc1794c04
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-05-31 16:08:38 +0900

    Update Missing Number.py

--- SoloLearn/Missing Number.py
+++ SoloLearn/Missing Number.py
@@ -1,9 +1,9 @@
 import  numpy as np
 import pandas as pd
-lst=[float(x) if x!='nan' else np.nan for x in input().split()]
-df0=pd.DataFrame(lst)
-df0.fillna(df0.mean(),inplace=True)
-df=df0.copy().applymap(lambda n:round(n,1))
-tp='dtype: '+str(df0[0].dtypes)
-print(df.to_string(header=False,col_space=4,float_format='{:.1f}'.format))
+lst=[float(x) if x!='nan' else np.NaN for x in input().split()]
+df=pd.DataFrame(lst)
+df.fillna(df[0].mean(),inplace=True)
+df.round(1)
+tp='dtype: '+str(df[0].dtypes)
+print(df.to_string(header=False,col_space=4,float_format='{:.1f}'.format,justify='justify'))
 print(tp)
