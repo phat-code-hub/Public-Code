ab6d7518ef727e53ea96a679367449cd17b17ec0
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-06-06 15:57:14 +0900

    update code
    Eternal Calendar

--- Algorithm/Gregory Calendar 1.py
+++ Algorithm/Gregory Calendar 1.py
@@ -1,29 +1,52 @@
+weekday_title=[['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+                ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']]
+days_of_month =[0,31,28,31,30,31,30,31,31,30,31,30,31]
+#--------------------------------------------------------------
 def IsleapYear(year):
     if year % 400 == 0 or (year % 4 ==0  and year % 100 !=0 ):
         return True
     else:
         return False
 #--------------------------------------------------------------
-def day_of_week(year,month):
-    days_of_month =[0,31,28,31,30,31,30,31,31,30,31,30,31]
+def day_of_week(year,month,opt_=1):
     if IsleapYear(year):
         days_of_month[2]=29
+    #January February  are considered 13nd and 14nd month
+    if month ==1 or month ==2:
+        year -=1
+        month +=12
     day=1
-    days=0
-    for year in range(1,year):
-        if IsleapYear(year):
-            days += 366
-        else:
-            days +=365
-    for m in range(1,month):
-        days += days_of_month[m]
-    days +=day
-    return days % 7
+    days=31+28+365*(year-1) + year//4 -year//100+year//400+ (306*(month+1)//10)-122 +day
+    ordDays=days % 7
+    if opt_ == 2:
+        ordDays -=1
+    return ordDays
+#--------------------------------------------------------------
+def show(year,month,dayOrd,start=1):
+    print (f'{month}/{year:0>4} Calendar:')
+    week_name=[w.rjust(3) for w in weekday_title[start-1]]
+    print(' '.join(week_name))
+    dNum=list(str(d) for d in range(1,days_of_month[month]+1))
+    for n in range(dayOrd):
+        dNum.insert(0,' ')
+    line,left=divmod(len(dNum),7)
+    if left>0:
+        for l in range(7-left):
+            dNum.insert(len(dNum),' ')
+        line +=1
+    lNum=[]
+    for l in range(line):
+        temp=[]
+        for n in range(7):
+            temp.append(dNum[l*7+n].rjust(3))
+        lNum.append(temp)
+    for i in range(line):
+        print(' '.join(lNum[i]))
 #--------------------------------------------------------------
 # Main Code:
 if __name__ == '__main__':
-    weekday_title=['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
-    weekday_names=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
     year=int(input('Year: '))
     month=int(input('Month: '))
-    print(weekday_names[day_of_week(year,month)])
+    opt=int(input('Starting weekday (1:Sunday ,2:Monday): '))
+    print()
+    show(year,month,day_of_week(year,month,opt),opt)
--- Algorithm/Gregory Calendar 2.py
+++ Algorithm/Gregory Calendar 2.py
@@ -1,6 +1,6 @@
 weekday_title=['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
-weekday_names=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 days_of_month =[0,31,28,31,30,31,30,31,31,30,31,30,31]
+#--------------------------------------------------------------
 def IsleapYear(year):
     if year % 400 == 0 or (year % 4 ==0  and year % 100 !=0 ):
         return True
@@ -10,15 +10,18 @@ def IsleapYear(year):
 def day_of_week(year,month):
     if IsleapYear(year):
         days_of_month[2]=29
+    #January February  are considered 13nd and 14nd month
+    if month ==1 or month ==2:
+        year -=1
+        month +=12
     day=1
     days=31+28+365*(year-1) + year//4 -year//100+year//400+ (306*(month+1)//10)-122 +day
     return (days % 7)
 #--------------------------------------------------------------
 def show(year,month,dayOrd):
-    print(dayOrd)
     print (f'{month}/{year} Calendar:')
-    Lich=[w.rjust(3) for w in weekday_title]
-    print(' '.join(Lich))
+    week_name=[w.rjust(3) for w in weekday_title]
+    print(' '.join(week_name))
     dNum=list(str(d) for d in range(1,days_of_month[month]+1))
     for n in range(dayOrd):
         dNum.insert(0,' ')
@@ -35,10 +38,10 @@ def show(year,month,dayOrd):
         lNum.append(temp)
     for i in range(line):
         print(' '.join(lNum[i]))
-    print(weekday_names[dayOrd])
 #--------------------------------------------------------------
 # Main Code:
 if __name__ == '__main__':
     year=int(input('Year: '))
     month=int(input('Month: '))
+    print()
     show(year,month,day_of_week(year,month))
