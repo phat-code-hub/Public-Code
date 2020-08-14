265f18f6623d1a8ec88229cc83a2fe943e37f3aa
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-08-14 22:29:24 +0900

    New Codes

---
+++ Spyder/test_val.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+v=tf.Variable(0,name='v')
+a=tf.constant(10,name='10')
+b=tf.constant(20,name='20')
+mul_op=tf.multiply(a,b,name='mul')
+assign_op=tf.compat.v1.assign(v,mul_op)
+sess=tf.compat.v1.Session()
+sess.run(assign_op)
+tf.compat.v1.summary.FileWriter('./log',sess.graph)
+res=sess.run(v)
+print(res)
