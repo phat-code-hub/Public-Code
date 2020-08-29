5b920fa69a150896b611ed7c29f6def020a8adc9
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-08-29 17:31:12 +0900

    Update Youtube_Link_Finder.java

--- Java Code/Youtube_Link_Finder.java
+++ Java Code/Youtube_Link_Finder.java
@@ -9,18 +9,15 @@ public class Youtube_Link_Finder {
 		Scanner sc =new Scanner(System.in);
 		try {
 			link_str=sc.nextLine().trim();
-			//link_str="https://youtu.be/KOOJDAO456w";
-			//link_str="http://www.youtube.com/watch?v=KOOJDAO456w";
 			String reg=".+[/|?v=]";
 			Pattern pat=Pattern.compile(reg);
 			Matcher mat=pat.matcher(link_str);
 			if (mat.find()) {
-				//int ind=mat.end();
 				System.out.println(link_str.substring(mat.end()));
 			}
-;		} finally {
+		}
+		finally {
 			sc.close();
 		}
+		}
 	}
-
-}
