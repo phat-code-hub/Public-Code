9c1c859417e4f5b0d9b9dc0c52b106e554ff6cda
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2020-09-29 22:02:55 +0900

    Create Dau.java

---
+++ Java Code/SoloLearn/Dau.java
@@ -0,0 +1,13 @@
+package SoloLearn;
+
+public class Dau {
+    public static void main (String[] args){
+        int i=1;
+        for (;i<6;i+=2){
+            i=i*i;
+            System.out.println(i);
+        }
+        System.out.println(i);
+    }
+   
+}
