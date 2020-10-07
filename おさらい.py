f6f940ea5a8c220e2f2bf251c939bfb5160d1bd1
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2020-10-07 22:16:45 +0900

    Update おさらい.py

--- おさらい.py
+++ おさらい.py
@@ -1,6 +1,12 @@
-if __name__ == "__main__":
-    x=1
-    for i in range(5):
-        x+=i
-    x+=1
-    print(X)
+def fun(*arg):
+    i=0
+    b=[]
+    for val in arg:
+        if val ==0:
+            # arg[i] =1
+            b.append(1)
+        else:
+            b.append(val)
+    
+    return b
+print(fun(5,2,0,4,0))
