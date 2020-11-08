a62e918c641598059821c6dcda7a830380c6c21b
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-11-08 18:48:48 +0900

    Update Vidu.py

--- Vidu.py
+++ Vidu.py
@@ -1,5 +1,12 @@
-x={1,2,3,4,5,6}
-y={0,1,2,3}&x
-print(y)
-z=list(filter(lambda c: c>1,y))
-print(z)
+l1={"blue","green","blue"}
+print(l1)
+l2={"green","blue","green"}
+print(l2)
+l1.add("black")
+print(l1)
+l3=list((lambda x,y: x-y) (l1,l2))
+print(l3[0][2:])
+if(2 % 2 == 0):
+    print(bool(1))
+else:
+    print(bool(0))
