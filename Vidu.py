72ea96c4a1f80bc7a933bfff48dfff05e936e2a2
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2020-11-09 20:08:35 +0900

    Update Vidu.py

--- Vidu.py
+++ Vidu.py
@@ -1,12 +1,16 @@
-l1={"blue","green","blue"}
-print(l1)
-l2={"green","blue","green"}
-print(l2)
-l1.add("black")
-print(l1)
-l3=list((lambda x,y: x-y) (l1,l2))
-print(l3[0][2:])
-if(2 % 2 == 0):
-    print(bool(1))
-else:
-    print(bool(0))
+# l1={"blue","green","blue"}
+# print(l1)
+# l2={"green","blue","green"}
+# print(l2)
+# l1.add("black")
+# print(l1)
+# l3=list((lambda x,y: x-y) (l1,l2))
+# print(l3[0][2:])
+# if(2 % 2 == 0):
+#     print(bool(1))
+# else:
+#     print(bool(0))
+a=[1,2,3]
+b=a
+del(a)
+print(b)
