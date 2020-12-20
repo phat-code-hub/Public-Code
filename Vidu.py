52196c0da0f13592cef748d173aa9ac80d471292
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2020-12-20 21:58:03 +0900

    Update Vidu.py

--- Vidu.py
+++ Vidu.py
@@ -1,6 +1,10 @@
-l=[]
-for i in range(17):
-    l.append(i*2)
-print(l)
-m=[x&1 for x in l]
-print(m)
+y=[]
+def loop(x):
+    for i in x:
+        try:
+            y.append(int(i))
+        except:
+            pass
+    return y
+z=loop(["2.0","1",y,abs(12j+1)])
+print(z)
