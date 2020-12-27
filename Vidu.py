6c1db8403866fb4b39ffc1bfd4c43b8146feb067
UEDA HATURU <59404113+phat-code-hub@users.noreply.github.com>
2020-12-27 22:38:44 +0900

    New Code

--- Vidu.py
+++ Vidu.py
@@ -1,2 +1,16 @@
-a=list(range(4))
-print(a[1::-3])
+# a=[8,0,4,6]
+# b=a.reverse()
+# d=reversed(a)
+# c=a[::-1]
+# print(a.reverse())
+# print(a[::-1])
+# print(b)
+# print(c)
+# print(d)
+a=7
+b=a>5 and 'a' or 'b'
+print(b)
+c='a' or 'b' and a>5
+print(c)
+d='b' or 'd' and a>5
+print(d)
