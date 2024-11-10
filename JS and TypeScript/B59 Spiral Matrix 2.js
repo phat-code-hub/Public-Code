974020108c7e60e320f875d07d1a6fe215719608
UEDA HATURU <phatnguyenduy@gmail.com>
2024-11-10 17:46:04 +0900

    Update B59 Spiral Matrix 2.js

--- JS and TypeScript/B59 Spiral Matrix 2.js
+++ JS and TypeScript/B59 Spiral Matrix 2.js
@@ -16,24 +16,3 @@ for (let loop = 0; loop<LIM;loop++){
 for (let j=0;j<n;j++){
     console.log(arr[j]);
 }
-
-
-
-// cnt = 1 # index from 1 to n
-// for loop in range((n + 1) // 2):
-//     # Top loop from Left to Right
-//     for i in range(loop, n - loop):
-//         arr[loop][i] = cnt
-//         cnt += 1
-//     # Right loop from top to bottom
-//     for i in range(loop + 1, n - loop):
-//         arr[i][n - loop - 1] = cnt
-//         cnt += 1
-//     # Bottom loop from right to left
-//     for i in range(n - loop - 2, loop - 1, -1):
-//         arr[n - loop - 1][i] = cnt
-//         cnt += 1
-//     # Left loop from bottom to top
-//     for i in range(n - loop - 2, loop, -1):
-//         arr[i][loop] = cnt
-//         cnt += 1
